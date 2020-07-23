a48999d41ca208c6783b317be5768e074bb6b258
Averiser <averiser@gmail.com>
2020-07-23 17:24:23 +0300

    Initial Commit

---
+++ MyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyQuiz
+//
+//  Created by MyMacBook on 23.07.2020.
+//  Copyright © 2020 MyMacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
